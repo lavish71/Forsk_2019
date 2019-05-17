96e23ef449d7175713a1909334e8883d9f05a17b
lavish71 <45160385+lavish71@users.noreply.github.com>
2019-05-17 16:43:30 +0530

    Add files via upload

---
+++ Forsk_Training/Code Challenges/Day_01/Practice_Day_01.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 14:41:49 2019
+
+@author: dell
+"""
+#1
+# Distance Travelled by vehilce(in km)
+dist = 100
+# After putting 5 litres of fuel(in liters) 
+fuel = 5
+# Formula of Average
+avg = dist/fuel
+# Average of my car
+print(avg)
+
+
+#2
+# Travelled dsitance(in KM)
+dist = 80
+# Cost of Diesel per litre(in INR)
+cost_diesel = 80
+# Vehicel Fuel Average
+average = 18
+# Cost of driving per km
+Cost_per_km = cost_diesel/average
+# Cost of driving per day
+day_cost = Cost_per_km * dist
+
+
+#3
+# 
